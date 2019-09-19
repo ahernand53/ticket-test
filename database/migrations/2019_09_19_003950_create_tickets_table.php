791005ceb94d6562f6243d0fcdf0289b9d8d3dcb
Asdrubal Hernandez <ahernand.development@gmail.com>
2019-09-19 18:03:41 -0500

    Agregando migraciones para los modelos

---
+++ database/migrations/2019_09_19_003950_create_tickets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('issue');
+            $table->string('priority');
+            $table->boolean('state');
+            $table->date('assignationDate');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+               $table->dropForeign('tickets_user_id_foreign');
+        });
+        Schema::dropIfExists('tickets');
+    }
+}
---
+++ database/migrations/2019_09_19_004221_create_ticket_details_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('task');
+            $table->boolean('state');
+            $table->string('concluding_remarks')->nullable();
+            $table->unsignedBigInteger('ticket_id');
+
+
+            $table->foreign('ticket_id')
+                ->references('id')
+                ->on('tickets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('ticket_details', function (Blueprint $table) {
+            $table->dropForeign('ticket_details_ticket_id_foreign');
+        });
+        Schema::dropIfExists('ticket_details');
+    }
+}
